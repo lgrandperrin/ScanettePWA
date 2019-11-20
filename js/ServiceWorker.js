158d24e6b3df8677cdddffae4b61a4a9be5833d2
Loïc Grandperrin <loic.grandperrin@outlook.fr>
2019-11-20 11:35:52 +0100

    Adding ServiceWorker

---
+++ js/ServiceWorker.js
@@ -0,0 +1,39 @@
+var cacheName = 'scanette';
+var appFiles = [
+    '/js/app.js',
+    '/js/DecoderWorker.js',
+    '/js/exif.js',
+    '/js/job.js',
+    '/js/ServiceWorker.js',
+    '/produit.csv',
+    '/style.css'
+];
+
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./ServiceWorker.js');
+};
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(appFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
